a9535f5af4b7ca25985cacf49883942f6624adff
Freddy <flguamanl@itsjapon.edu.ec>
2025-03-05 18:04:01 -0500

    xd

--- EcoConexiones/src/utils/validation.ts
+++ EcoConexiones/src/utils/validation.ts
@@ -1,13 +1,8 @@
-export const validatePassword = (password: string): { isValid: boolean; errors: string[] } => {
-  const errors: string[] = [];
 
   if (password.length < 8) {
     errors.push('La contraseña debe tener al menos 8 caracteres');
   }
 
-  if (!/[A-Z]/.test(password)) {
-    errors.push('La contraseña debe contener al menos una mayúscula');
-  }
 
   if (!/[a-z]/.test(password)) {
     errors.push('La contraseña debe contener al menos una minúscula');
@@ -16,6 +11,12 @@ export const validatePassword = (password: string): { isValid: boolean; errors: 
   if (!/[0-9]/.test(password)) {
     errors.push('La contraseña debe contener al menos un número');
   }
+
+
+
+
+
+
 
   if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
     errors.push('La contraseña debe contener al menos un carácter especial');
